3b7defcc62953dd4c7fdec89404844c5265fb372
lumnikemel <gswrutter@gmail.com>
2021-12-14 00:36:40 -0600

    Create ArchInstall-00.1-ConsoleInstructions.sh

---
+++ Installation/ArchInstall-00.1-ConsoleInstructions.sh
@@ -0,0 +1,27 @@
+# Run these commands at the ArchIso console:
+
+# Initial setup required from PC/VM console:
+## Set password
+echo -e "password\npassword" | sudo -S passwd
+
+## Start sshd
+sudo systemctl start sshd
+
+DISKS=$(ls -la /dev/disk/by-id | grep "../" | cut -f 11-13 -d " ")
+
+IPADDRESS=$(ip a | grep "inet " | grep -v host | cut -f 6 -d " " | cut -f 1 -d "/")
+
+echo "\nAvailable disks are:\n$DISKS"
+echo "\nIP Address is: ${IPADDRESS}"
+echo "Command to SSH in is: ssh root@${IPADDRESS}"
+
+pacman -Sy --noconfirm git
+cd /root
+rm -rf ArchInstall
+git clone https://gist.github.com/bcfbfdc7645cf5b7401cdbe229d84d98.git ArchInstall
+cd ArchInstall
+chmod +x *
+SCRIPT_DIR=/root/ArchInstall
+. $SCRIPT_DIR/vars
+
+./ArchInstall-00-RunMeFirst.sh
