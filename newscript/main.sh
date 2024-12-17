6eda7b46dde4c1e2b2fa90c5d84dc046768eb078
lumnikemel <gswrutter@gmail.com>
2024-12-17 14:29:59 -0600

    Create main.sh

---
+++ newscript/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/lumnikemel/ArchAngel/refs/heads/main/newscript/archiso.sh | bash
+curl -s https://raw.githubusercontent.com/lumnikemel/ArchAngel/refs/heads/main/newscript/chroot.sh | /mnt/tmp/chroot.sh && arch-chroot /mnt /bin/bash /tmp/chroot.sh
+
+# Reboot
+umount /mnt/boot
+umount /mnt
+
+#reboot now
+
